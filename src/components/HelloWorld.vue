b18787174779601896eb13b6022bb223e58d99d2
BigDong <1106339848@qq.com>
2022-11-01 11:46:15 +0800

    fix:测试lint-staged/vue文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="test">123123</h1>
+  <h1 class="test"></h1>
   <!-- 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -20,7 +20,7 @@
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
     in your IDE for a better DX
   </p> -->
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">C o learn more</p>
 </template>
 <script setup lang="ts"></script>
 <style lang="less" scoped>
